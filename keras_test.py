8977071264bdfd0d545d7fa2b27f83b4f96fff8f
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2017-12-01 19:18:32 +0900

    modify keras_test.py

--- keras_test.py
+++ keras_test.py
@@ -6,8 +6,7 @@ from tensorflow.python.keras import backend as K
 
 from mlp.model import MLP
 
-def load_data():
-	dirn = './MNIST_data'
+def load_data(dirn):
 	data = mnist.read_data_sets(dirn, one_hot=True)
 
 	print(data.train.num_examples, 'train samples')
@@ -17,7 +16,7 @@ def load_data():
 	return data
 
 if __name__ == '__main__':
-	mnist = load_data()
+	mnist = load_data('./MNIST_data')
 	# tensorflow placeholders
 	x = tf.placeholder(tf.float32, [None, 784])
 	y_ = tf.placeholder(tf.float32, [None, 10])
@@ -36,15 +35,16 @@ if __name__ == '__main__':
 		print('Training...')
 		for i in range(10001):
 			batch_xs, batch_ys = mnist.train.next_batch(100)
-			train_fd = {x: batch_xs, y_: batch_ys, K.learning_phase(): 1}
+			train_fd = { x: batch_xs, y_: batch_ys, K.learning_phase(): 1 }
 			train_step.run(feed_dict=train_fd)
 			if i % 1000 == 0:
 				batch_xv, batch_yv = mnist.test.next_batch(200)
 				val_accuracy = accuracy.eval({x: batch_xv, y_: batch_yv, K.learning_phase(): 0})
 				print('\tstep, accurary = %6d: %6.3f' % (i, val_accuracy))
 
-		test_fd = {x: mnist.test.images, y_: mnist.test.labels,
-					K.learning_phase(): 0}
+		test_fd = { x: mnist.test.images,
+					y_: mnist.test.labels,
+					K.learning_phase(): 0 }
 		test_accuracy = accuracy.eval(feed_dict=test_fd)
 		print('Test accuracy:', test_accuracy)
 
